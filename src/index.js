1b8e88ed434def5a483f1f104244dcdf625bb933
davieu <davisumana@gmail.com>
2021-04-26 00:33:01 -0400

    (2:58:52​) Event Basics

--- src/index.js
+++ src/index.js
@@ -38,29 +38,59 @@ function BookList() {
         // const { img, title, author } = book;
         // return <Book key={book.id} book={book}></Book>;
         // spreading the book object with ...
-        return <Book key={book.id} book={book} {...book}></Book>;
+        return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );
 }
 
 //function Book({img, title, author}) //{ //js destructuring from the parameters
-function Book(props) {
+function Book({ img, title, author }) {
   // const { img, title, author } = props; //js destructuring
-  const { img, title, author } = props;
-  console.log(props);
-  return (
-    <article className='book'>
-      {/* This is the non destructured rendering of the book object */}
-      <img src={props.book.img} alt='' />
-      <h1>{props.book.title}</h1>
-      <h4>{props.book.author}</h4>
+  // attribute, eventhandler
+  // onClick, onMouseOVer
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    alert('hello');
+  };
 
-      {/* destructred book object */}
+  const complexExample = (author) => {
+    console.log(author);
+  };
+
+  return (
+    <article
+      className='book'
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {/* <h4>{props.book.children}</h4> */}
+      <button type='button' onClick={clickHandler}>
+        reference example
+      </button>
+      {/* will run when you click it. */}
+      <button type='button' onClick={() => complexExample('author: ' + author)}>
+        more complex example
+      </button>
+      {/* this button will run on page load. the above will run on click */}
+      <button type='button' onClick={complexExample(author)}>
+        Will run ON PAGE LOAD
+      </button>
+      <button
+        type='button'
+        onClick={() => {
+          console.log(title);
+        }}
+      >
+        reference example2 TITLE
+      </button>
+      <button type='button' onClick={() => console.log(author)}>
+        reference example3 AUTHOR
+      </button>
     </article>
   );
 }
